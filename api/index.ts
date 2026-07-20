1226f26d3c494472d6592d5d53fdb245ea339e6d
ssinggih <singgipenaraga@gmail.com>
2026-07-20 08:02:59 +0800

    fix: vercel build - remove outputDirectory, add tsconfig-paths/register

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,4 @@
+import 'tsconfig-paths/register';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
